131877a49aaece736a1734a2bd67482e2cf4e646
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-07-30 13:10:41 +0530

    added: employee wage full time

--- src/com/bl/EmployeeWage.java
+++ src/com/bl/EmployeeWage.java
@@ -3,15 +3,25 @@ package com.bl;
 public class EmployeeWage {
 
     int is_full_time = 1;
+    int per_hrs_rate = 20;
+    int emphrs;
+    int empwage;
     public static void main(String[] args) {
         EmployeeWage obj = new EmployeeWage();
+
       int empCheck = (int)(Math.random()*10%2);
         System.out.println(empCheck);
+
       if (empCheck == obj.is_full_time){
-          System.out.println("employee is present");
+           obj.emphrs = 8;
       }else {
-          System.out.println("employee not present");
+          obj.emphrs = 0;
+
       }
+
+      obj.empwage = obj.emphrs * obj.per_hrs_rate;
+
+        System.out.println(obj.empwage);
 
     }
 
